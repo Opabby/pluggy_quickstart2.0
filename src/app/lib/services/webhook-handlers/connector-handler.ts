0bd5c53ba6611cfe407c19b5cee9280c0fa2c0d0
Thaís Rodeiro <opabby82@gmail.com>
2025-12-16 08:27:02 -0300

    removing connector handler

--- src/app/lib/services/webhook-handlers/connector-handler.ts
+++
@@ -1,6 +0,0 @@
-import { ConnectorStatusWebhookPayload } from "../../../types/pluggy";
-
-export async function handleConnectorStatusUpdate(payload: ConnectorStatusWebhookPayload): Promise<void> {
-  const { connectorId, data } = payload;
-  console.log(`Connector ${connectorId} status updated:`, data);
-}
